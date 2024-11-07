594285d6f0a61539e409d56caf0a2b4bbc5ee0c5
akiyamajyunta <phononiwasi@gmail.com>
2024-11-07 21:41:32 +0900

    https://github.com/akiyamajyunta/hokkaidougourmet.git

--- page.js
+++
@@ -1,9 +0,0 @@
-function humbergerbasyo() {
-  var element = document.getElementById("humberger-basyo");
-  element.style.position = 'fixed ';
-  element.style.right = `${500}px`;
-}
-humbergerbasyo();
-
-alert("作動してます");
-
